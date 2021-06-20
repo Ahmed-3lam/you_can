576c49ead8edc32bd478c1b9bcfaec2d9bdb0cbe
Ahmed Allam <3lam.ahmedd@gmail.com>
2021-06-20 23:55:14 +0200

    WorkoutView

---
+++ lib/WoroutView/WorkoutScreen.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+
+class WorkOutScreen extends StatefulWidget {
+  const WorkOutScreen({key}) : super(key: key);
+
+  @override
+  _WorkOutScreenState createState() => _WorkOutScreenState();
+}
+
+
+class _WorkOutScreenState extends State<WorkOutScreen> {
+  String btnText = "Start Training";
+
+  bool trainingEnable = true;
+  String trainingImage = "Tummy.png"  ;
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+
+      body: Column(
+
+        children: [
+
+             Container(
+              width: double.infinity,
+              height: 300,
+              decoration: BoxDecoration(
+                color: primaryColor,
+                image: new DecorationImage(
+                  image: new ExactAssetImage('assets/images/$trainingImage'),
+              ),
+    borderRadius: BorderRadius.only(
+      bottomLeft: Radius.circular(40),
+      bottomRight: Radius.circular(40),
+    )
+            )
+            ),
+          SizedBox(height: 150,),
+          Container(
+            width: 300,
+            height: 70,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              color: primaryColor,
+            ),
+            child: MaterialButton(
+              onPressed: () {
+                setState(()
+                {
+                  if(trainingEnable){
+                    trainingImage = "Seated.png";
+                    trainingEnable=false;
+                    btnText="One";
+
+                  }
+                  else{
+
+                    trainingImage = "Tummy.png";
+                    trainingEnable=true;
+                    btnText="Two";
+                  }
+
+                });
+
+
+              },
+              child: Text(
+                '$btnText',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25
+                ),
+              ),
+            ),
+          ),
+
+          
+
+
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/WoroutView/colors.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+Color primaryColor =Color.fromRGBO(76, 104, 218, 1);
