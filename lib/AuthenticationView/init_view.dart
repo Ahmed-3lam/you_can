0ae55c4cad648f64203334523dcb8f1800c8901f
Ahmed Allam <3lam.ahmedd@gmail.com>
2021-06-23 16:07:28 +0200

    Login & Sign Up

--- lib/AuthenticationView/init_view.dart
+++ lib/AuthenticationView/init_view.dart
@@ -5,9 +5,70 @@ class InitView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    Color primaryColor =Color.fromRGBO(76, 104, 218, 1);
+    return Scaffold(
+        backgroundColor: Colors.white,
 
-        /// ToDo add initial page here Ahemd
-        );
+
+        body: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: SingleChildScrollView(
+              child:Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+
+                children: [
+                  Image.asset("assets/images/board3.jpg",height: 300, width: 300,),
+                  SizedBox(height: 50,),
+                  Container(
+                    width: double.infinity,
+                    height: 70,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      color: primaryColor,
+                    ),
+                    child: MaterialButton(
+                      onPressed: () {
+
+                      },
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 30,),
+                  Container(
+                    width: double.infinity,
+                    height: 70,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      color: primaryColor,
+                    ),
+                    child: MaterialButton(
+                      onPressed: () {
+
+                      },
+                      child: Text(
+                        'Sign Up',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25
+                        ),
+                      ),
+                    ),
+                  ),
+
+
+
+                ],
+              ),
+            )
+
+
+        )
+    );
   }
 }
